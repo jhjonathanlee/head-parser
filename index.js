a18a3ab936447d09dfb070c35ebf3e6e8554e225
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-10-04 16:37:15 -0400

    response with parsed request header available through /api/whoami

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/whoami', (req, res) => {
+  var headers = {};
+  headers.ipaddress = req.ip;
+  headers.user_agent = req.get('User-Agent');
+  headers.language = req.get('Accept-Language');
+  res.json(headers);
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
